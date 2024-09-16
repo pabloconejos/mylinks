15319f0a2edc4a1aaddbe98dd70f74c5bd4a6ae1
Pablo Conejos <pconejos03@gmail.com>
2024-09-16 21:11:05 +0200

    Skeleton cards

---
+++ src/app/shared/profile-card-skeleton/profile-card-skeleton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-card-skeleton',
+  templateUrl: './profile-card-skeleton.component.html',
+  styleUrl: './profile-card-skeleton.component.scss'
+})
+export class ProfileCardSkeletonComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,15 +7,16 @@ import { RouterModule } from '@angular/router';
 import { NotificationComponent } from './notification/notification.component';
 import { LinkComponent } from './link/link.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProfileCardSkeletonComponent } from './profile-card-skeleton/profile-card-skeleton.component';
 
 
 
 @NgModule({
-  declarations: [LoaderComponent, HeaderComponent, SelectBackgroundComponent, NotificationComponent, LinkComponent, FooterComponent],
+  declarations: [LoaderComponent, HeaderComponent, SelectBackgroundComponent, NotificationComponent, LinkComponent, FooterComponent, ProfileCardSkeletonComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [LoaderComponent, HeaderComponent, SelectBackgroundComponent, NotificationComponent, LinkComponent, FooterComponent]
+  exports: [LoaderComponent, HeaderComponent, SelectBackgroundComponent, NotificationComponent, LinkComponent, FooterComponent, ProfileCardSkeletonComponent]
 })
 export class SharedModule { }
